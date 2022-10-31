d0efccc4a2d6dfd80716c6fcae1042d7319a12f1
Diego Fernando Alayon <dfar.41@gmail.com>
2022-10-31 12:09:18 -0500

    agreando componente de Title

---
+++ src/components/Title/index.js
@@ -0,0 +1,12 @@
+import { Component } from 'react'
+import './title.css'
+class Title extends Component {
+  render() {
+    return(
+      <>
+        <h1 className='title-styles'>Tienda</h1>
+      </>
+    )
+  }
+}
+export default Title
