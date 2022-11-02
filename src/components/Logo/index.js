26bb7925c6908cd139bc89f6da8f1fff7473f3cb
Diego Fernando Alayon <dfar.41@gmail.com>
2022-11-02 13:17:01 -0500

    creando Logo

---
+++ src/components/Logo/index.js
@@ -0,0 +1,14 @@
+import { Component } from 'react'
+
+class Logo extends Component {
+
+  render() {
+    return(
+      <>
+        <h1>Logo</h1>
+      </>
+    )
+  }
+}
+
+export default Logo
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,11 +1,12 @@
 import { Component } from 'react'
+import Logo from '../Logo'
 import './navbar.css'
 class Navbar extends Component {
 
   render() {
     return(
       <nav className='nav-bar'>
-        <p>Logo</p>
+        <Logo />
         <p>Carro</p>
       </nav>
     )
