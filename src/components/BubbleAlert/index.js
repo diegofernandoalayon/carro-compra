9fad4e15c22ac985cc0119f4b58ac2ce3ff7b912
Diego Fernando Alayon <dfar.41@gmail.com>
2022-11-04 22:37:30 -0500

    agregando estilos a BubbleAlert

--- src/components/BubbleAlert/index.js
+++ src/components/BubbleAlert/index.js
@@ -1,12 +1,12 @@
 import { Component } from 'react'
-
+import './bubblealert.css'
 class BubbleAlert extends Component {
 
   render(){
     return(
-      <>
+      <span className='bubbleAlert'>
         1
-      </>
+      </span>
     )
   }
 }
