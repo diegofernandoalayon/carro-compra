433f14ece0666e9bdabfcca477e023ef544d10d4
Diego Fernando Alayon <dfar.41@gmail.com>
2022-11-09 18:51:58 -0500

    funcionalidad boton carro

--- src/components/Carro/index.js
+++ src/components/Carro/index.js
@@ -3,7 +3,9 @@ import BubbleAlert from '../BubbleAlert'
 import DetallesCarro from '../DetallesCarro'
 import './carro.css'
 class Carro extends Component {
-
+  state = {
+    showCar : false
+  }
   render() {
     const { carro } = this.props
     const cantidad = carro.reduce((acc, el) => acc + el.cantidad, 0)
@@ -15,10 +17,13 @@ class Carro extends Component {
           }
           
         </span>
-        <button className='carro'>
+        <button className='carro' onClick={() => this.setState({showCar : !this.state.showCar})}>
           Card
         </button>
-        <DetallesCarro carro = {carro} />
+        {
+          this.state.showCar && <DetallesCarro carro = {carro} />
+        }
+        
       </div>
     )
   }
